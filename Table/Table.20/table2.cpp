f6aad0ab7338f5d33489c0fd089e2a74b7039698
HiiDrons <yar.pal.lipatov@gmail.com>
2025-08-28 16:23:22 +0400

    Add files via upload

---
+++ Table/Table.20/table2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    int size;
+
+    std::cout << "Введите размер таблицы:";
+    std::cin >> size;
+
+    std::cout << "\033[1;32m";
+    std::cout << "     |";
+    for (int col = 1; col <= size; ++col) {
+        std::cout << std::setw(4) << col;
+    }
+    std::cout << "\n";
+
+    std::cout << "-----+";
+    for (int col = 1; col <= size; ++col) {
+        std::cout << "----";
+    }
+    std::cout << "\n\033[0m"; 
+
+    for (int row = 1; row <= size; ++row) {
+        std::cout << "\033[1;34m" << std::setw(4) << row << "\033[0m|";
+
+        for(int col = 1; col <= size; ++col) {
+            if(row == col)
+                std::cout << "\033[1;31m";
+
+            std::cout << std::setw(4) << row * col;
+
+            std::cout << "\033[0m"; // сброс цвета после каждого числа
+        }
+
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
+ 
